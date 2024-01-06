9f3d880c99bf9528125c85524011ed39f39f1e75
RaminMajidi <raminmajididev01@gmail.com>
2024-01-06 23:41:17 +0330

    change the paginationHandler

--- server/lib/utils/paginationHandler.js
+++ server/lib/utils/paginationHandler.js
@@ -4,15 +4,17 @@ const paginationHandler = (req, res, count) => {
     let limit = 5;
     const page = +req.query.page
     if (!page) {
-        return errorHandler(res, 400, 'درخواست نادرست است !')
+        return errorHandler(res, 400, 'صفحه درخواست شده نامعتبر است !')
     }
 
     if (+req.query.limit > 5 && +req.query.limit <= 20) {
         limit = +req.query.limit
     }
 
-
-    let sort = req.query.sort || 'asc'
+    if (req.query.sort && req.query.sort != "asc" && req.query.sort != "desc") {
+        return errorHandler(res, 400, 'درخواست مرتب سازی نامعتبر است !')
+    }
+    let sort = req.query.sort || "asc";
     let offset = ((page - 1) * limit);
     let totalPage = Math.ceil((count / limit))
 
