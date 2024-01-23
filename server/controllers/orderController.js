50391bcf1fd9f1c6dd04b3591f7c628cc9ec5dfa
RaminMajidi <raminmajididev01@gmail.com>
2024-01-23 14:00:47 +0330

    minor change

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -49,119 +49,119 @@ exports.getOredrById = async (req, res, next) => {
 }
 
 
-// exports.postNewOrder = async (req, res, next) => {
-//     try {
+exports.postNewOrder = async (req, res, next) => {
+    try {
 
-//         const checkOrder = await Order.findOne({
-//             where: { userId: req.userId, status: 'REGISTERED' }
-//         })
+        const checkOrder = await Order.findOne({
+            where: { userId: req.userId, status: 'REGISTERED' }
+        })
 
-//         if (checkOrder) {
-//             return errorHandler(res, 400, "شما سفارش پرداخت نشده دارید ابتدا وضعیت آن را مشخص و مجددا سفارش ثبت کنید !")
-//         }
+        if (checkOrder) {
+            return errorHandler(res, 400, "شما سفارش پرداخت نشده دارید ابتدا وضعیت آن را مشخص و مجددا سفارش ثبت کنید !")
+        }
 
-//         const valid = await validationHandler(req, next)
-//         if (valid) {
-//             let whereId = []
-//             for (let q in req.body.products) {
-//                 whereId.push(req.body.products[q].productId)
-//             }
+        const valid = await validationHandler(req, next)
+        if (valid) {
+            let whereId = []
+            for (let q in req.body.products) {
+                whereId.push(req.body.products[q].productId)
+            }
 
-//             const products = await Product.findAll({
-//                 where: { id: { [Op.or]: whereId } },
-//                 attributes: ['id', 'price', 'quantity']
-//             })
+            const products = await Product.findAll({
+                where: { id: { [Op.or]: whereId } },
+                attributes: ['id', 'price', 'quantity']
+            })
 
-//             let totatPrice = 0
-//             products.map((item, index) => {
-//                 totatPrice += req.body.products[index].quantity * item.price
-//             })
+            let totatPrice = 0
+            products.map((item, index) => {
+                totatPrice += req.body.products[index].quantity * item.price
+            })
 
-//             const { state, city, address, zip } = req.body
+            const { state, city, address, zip } = req.body
 
-//             const newOrder = await Order.create({
-//                 userId: req.userId,
-//                 totalPrice: totatPrice,
-//                 state: state,
-//                 city: city,
-//                 address: address,
-//                 zip: zip
-//             })
+            const newOrder = await Order.create({
+                userId: req.userId,
+                totalPrice: totatPrice,
+                state: state,
+                city: city,
+                address: address,
+                zip: zip
+            })
 
-//             let items = []
-//             await req.body.products.map((item, index) => {
-//                 let obj = {
-//                     orderId: newOrder.id,
-//                     productId: item.productId,
-//                     quantity: item.quantity,
-//                     price: products[index].price
-//                 }
-//                 items.push(obj)
-//             })
+            let items = []
+            await req.body.products.map((item, index) => {
+                let obj = {
+                    orderId: newOrder.id,
+                    productId: item.productId,
+                    quantity: item.quantity,
+                    price: products[index].price
+                }
+                items.push(obj)
+            })
 
-//             await OrderItem.bulkCreate(items)
-//             res.status(201).json({ orderId: newOrder.id, message: "سفارش با موفقیت ثبت شد ." })
-//         }
-//     } catch (error) {
-//         next(error)
-//     }
-// }
+            await OrderItem.bulkCreate(items)
+            res.status(201).json({ orderId: newOrder.id, message: "سفارش با موفقیت ثبت شد ." })
+        }
+    } catch (error) {
+        next(error)
+    }
+}
 
-// exports.getPayment = async (req, res, next) => {
-//     try {
-//         const order = await Order.findOne({
-//             where: { id: req.params.id },
-//             attributes: ['id', 'totalPrice']
-//         })
+exports.getPayment = async (req, res, next) => {
+    try {
+        const order = await Order.findOne({
+            where: { id: req.params.id },
+            attributes: ['id', 'totalPrice']
+        })
 
-//         zarinpal.PaymentRequest({
-//             Amount: order.totalPrice,
-//             CallbackURL: `${req.protocol}://${req.get('host')}/checkPayment`,
-//             Description: 'پرداخت صورت حساب',
-//             Email: req.userEmail
-//         }).then(response => {
-//             console.log(response);
-//             if (response.status == 100) {
-//                 res.redirect(response.url)
-//             }
-//         }).catch(err => next(err))
+        zarinpal.PaymentRequest({
+            Amount: order.totalPrice,
+            CallbackURL: `${req.protocol}://${req.get('host')}/checkPayment`,
+            Description: 'پرداخت صورت حساب',
+            Email: req.userEmail
+        }).then(response => {
+            console.log(response);
+            if (response.status == 100) {
+                res.redirect(response.url)
+            }
+        }).catch(err => next(err))
 
-//     } catch (error) {
-//         next(error)
-//     }
-// }
+    } catch (error) {
+        next(error)
+    }
+}
 
 
-// exports.checkPayment = async (req, res, next) => {
-//     console.log(req.query);
-//     const authority = req.query.Authority;
-//     const status = req.query.Status;
+exports.checkPayment = async (req, res, next) => {
+    console.log(req.query);
+    const authority = req.query.Authority;
+    const status = req.query.Status;
 
-//     const order = await Order.findOne({
-//         where: { userId: req.userId, status: "REGISTERED" },
-//         attributes: ['id', 'totalPrice'],
-//         include: [{
-//             model: OrderItem,
-//             attributes: ['id', 'productId', 'quantity'],
-//         }],
-//     })
+    const order = await Order.findOne({
+        where: { userId: req.userId, status: "REGISTERED" },
+        attributes: ['id', 'totalPrice'],
+        include: [{
+            model: OrderItem,
+            attributes: ['id', 'productId', 'quantity'],
+        }],
+    })
 
-//     if (status == 'OK') {
-//         zarinpal.PaymentVerification({
-//             Amount: order.totalPrice,
-//             Authority: authority
-//         }).then(response => {
-//             if (response.status == 100) {
-//                 const updateOrder = Order.update(
-//                     { refId: response.RefID, status: "PAID" },
-//                     { where: { id: order.id } })
-//             }
-//         }).then(() => {
-//             res.status(200).json({ message: "پرداخت موفقیت آمیز بود" })
-//         }).catch(err => next(err))
-//     }
+    if (status == 'OK') {
+        zarinpal.PaymentVerification({
+            Amount: order.totalPrice,
+            Authority: authority
+        }).then(response => {
+            if (response.status == 100) {
+                const updateOrder = Order.update(
+                    { refId: response.RefID, status: "PAID" },
+                    { where: { id: order.id } })
+            }
+        }).then(() => {
+            res.status(200).json({ message: "پرداخت موفقیت آمیز بود" })
+        }).catch(err => next(err))
+    }
 
-// }
+}
 
 
 
